a6da8716e9b1dbe931b8a8540a03cb4fcd1453d3
Redwan Rahman <redwanrahman2002@outlook.com>
2026-02-17 14:22:31 +0600

    Remove unnecessary blank line and improve output formatting in main function

--- dartEvaluation01/evaluation/q2.dart
+++ dartEvaluation01/evaluation/q2.dart
@@ -5,7 +5,6 @@ class bankAccount {
   bankAccount(this.holderName, this.balance);
 
 }
-
 class savingsAccount extends bankAccount {
   
   savingsAccount(String holderName, double balance) : super(holderName, balance);
@@ -26,6 +25,11 @@ void main(){
   savingsAccount account = savingsAccount('Redwan Rahman', 242220005101127);
 
 
+  
+  print("Account holder: ${account.holderName}");
+  print("Current balance: ${account.balance}");
   account.withdraw(1127.0);
+  print('Balance after withdrawal: ${account.balance}');
+  account.deposit(5000.0);
   print('Balance after deposit: ${account.balance}');
 }
