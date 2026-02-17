769c40ff138765f5fe8cbbb0581ad0a2ca593490
Redwan Rahman <redwanrahman2002@outlook.com>
2026-02-17 13:49:45 +0600

    Add initial implementation of grading functions and main logic

---
+++ dartEvaluation01/evaluation/q1.dart
@@ -0,0 +1,27 @@
+double calculateTotal(double marks1, double marks2, double marks3) {
+  double total = marks1 + marks2 + marks3;
+  return total;
+}
+double calculateAverage(double total){
+  double average = total/3;
+  return average;
+}
+bool isPassed(double average){
+  if(average >= 5.0){
+    return true;
+  } else {
+    return false;
+  }
+}
+void main(){
+  // last four digits of student ID: 1127
+  double marks1 = 7.0;
+  double marks2 = 2.0;
+  double marks3 = 11.0;
+  double total = calculateTotal(marks1, marks2, marks3);
+  double average = calculateAverage(total);
+  bool passed = isPassed(average);
+  print('Total Marks: $total');
+  print('Average Marks: $average');
+  print('Passed: $passed');
+}
